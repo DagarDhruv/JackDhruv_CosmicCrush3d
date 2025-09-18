375734ac81148d3b0491d9ec0c369065c0cb7e6a
Jack Kane <jkane2207@gmail.com>
2025-09-18 09:44:57 +0100

    added lab1 as starting point as lab2 is similar

--- CosmicCrush/Assets/Scripts/PlayerController.cs
+++ CosmicCrush/Assets/Scripts/PlayerController.cs
@@ -1,46 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+
 public class PlayerController : MonoBehaviour
 {
+    // Rigidbody of the player.
     private Rigidbody rb;
-    private int count = 0;
+
+    // Variable to keep track of collected "PickUp" objects.
+    private int count;
+
+    // Movement along X and Y axes.
     private float movementX;
     private float movementY;
+
+    // Speed at which the player moves.
     public float speed = 0;
+
+    // UI text component to display count of "PickUp" objects collected.
     public TextMeshProUGUI countText;
+
+    // UI object to display winning text.
     public GameObject winTextObject;
-    // Start is called before the first frame update
+
+    // Start is called before the first frame update.
     void Start()
     {
-        count = 0;
+        // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
+
+        // Initialize count to zero.
+        count = 0;
+
+        // Update the count display.
         SetCountText();
+
+        // Initially set the win text to be inactive.
         winTextObject.SetActive(false);
     }
-    private void FixedUpdate()
+
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
-    }
-    void OnMove (InputValue momentValue)
-    {
-        Vector2 movementVector = momentValue.Get<Vector2>(); 
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        // Store the X and Y components of the movement.
         movementX = movementVector.x;
         movementY = movementVector.y;
-
     }
-    void SetCountText()
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 26)
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+        if (other.gameObject.CompareTag("PickUp"))
         {
-            winTextObject.SetActive(true);
+            // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+
+            // Increment the count of "PickUp" objects collected.
+            count = count + 1;
+
+            // Update the count display.
+            SetCountText();
         }
     }
 
-    // Update is called once per frame
+    // Function to update the displayed count of "PickUp" objects collected.
+    void SetCountText()
+    {
+        // Update the count text with the current count.
+        countText.text = "Count: " + count.ToString();
 
+        // Check if the count has reached or exceeded the win condition.
+        if (count >= 12)
+        {
+            // Display the win text.
+            winTextObject.SetActive(true);
+        }
+    }
 }
